6e3c9f8796d817885c39dc0572cfe9ef6428d58a
Masaya Konishi <konishi@office-ore.net>
2013-12-16 02:04:57 +0900

    メールサーバーのレシピひな形を追加

---
+++ site-cookbooks/postfix/README.md
@@ -0,0 +1,13 @@
+# postfix cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ site-cookbooks/postfix/metadata.rb
@@ -0,0 +1,8 @@
+name             'postfix'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures postfix'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
