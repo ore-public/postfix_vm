4f716b3cea7493a63e92604d9c7360bf3320df84
Masaya Konishi <konishi@office-ore.net>
2013-12-30 17:15:30 +0900

    appにsshでログイン出来るようにした

--- postfix-chef-repo/site-cookbooks/app/recipes/default.rb
+++ postfix-chef-repo/site-cookbooks/app/recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+group "app" do
+  group_name "app"
+  action :create
+end
+
+
+user "app" do
+  group "app"
+  action :create
+end
+
+directory "/home/app/.ssh" do
+  owner "app"
+  group "app"
+  mode 0700
+  action :create
+end
+
+template "/home/app/.ssh/authorized_keys" do
+  user "app"
+  group "app"
+  mode 0600
+end
+
